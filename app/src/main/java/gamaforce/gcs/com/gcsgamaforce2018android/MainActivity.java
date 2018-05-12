f0723f97e7d9bc9174cf7525da3958c0f092b6c5
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2018-05-12 11:30:40 +0700

    - Added usb serial driver library - Still not tested yet - I'll clean up the code later

--- app/src/main/java/gamaforce/gcs/com/gcsgamaforce2018android/MainActivity.java
+++ app/src/main/java/gamaforce/gcs/com/gcsgamaforce2018android/MainActivity.java
@@ -1,7 +1,15 @@
 package gamaforce.gcs.com.gcsgamaforce2018android;
 
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get UsbManager from Android.
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        // Find the first available driver.
+        UsbSerialDriver driver = UsbSerialProber.acquire(manager);
+
+        if (driver != null) {
+            try {
+                driver.open();
+                driver.setBaudRate(115200);
+
+                byte buffer[] = new byte[16];
+                int numBytesRead = driver.read(buffer, 1000);
+                Log.d("UsbSerialDriver", "Read " + numBytesRead + " bytes.");
+            } catch (IOException e) {
+                // Deal with error.
+            } finally {
+                try {
+                    driver.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
